59a132fef41a72bcd040f191dfe2999f2f599598
yaochao20 <yaochao20@huawei.com>
2026-03-16 16:19:24 +0800

    [fix] fix retool script run_qwen2_7b_dapo execution failed.TypeError: cannot pickle '_contextvars.ContextVar' object

--- retool/retool.py
+++ retool/retool.py
@@ -22,6 +22,7 @@ from verl.tools.sandbox_fusion_tools import SandboxFusionTool
 from verl.utils.dataset import RLHFDataset
 from verl.utils.reward_score import math_dapo
 from verl.utils.rollout_trace import rollout_trace_op
+from recipe.retool.dataset_utils import map_fn, map_fn2
 
 logger = logging.getLogger(__name__)
 
@@ -58,9 +59,6 @@ class CustomSandboxFusionTool(SandboxFusionTool):
         return result, None, None
 
 
-answer_format = """\nThe answer format must be: \\boxed{'The final answer goes here.'}"""
-
-
 class CustomRLHFDataset(RLHFDataset):
     """Custom dataset class to process Maxwell-Jia/AIME_2024, yentinglin/aime_2025 datasets."""
 
@@ -72,36 +70,14 @@ class CustomRLHFDataset(RLHFDataset):
             data_source = "/".join(parquet_file.split("/")[-2:])
             if data_source in ["Maxwell-Jia/AIME_2024", "yentinglin/aime_2025"]:
                 dataframe = dataframe.map(
-                    self.map_fn, fn_kwargs={"data_source": data_source}, remove_columns=dataframe.column_names
+                    map_fn, fn_kwargs={"data_source": data_source}, remove_columns=dataframe.column_names
                 )
             else:
-                dataframe = dataframe.map(self.map_fn2, num_proc=16)
+                dataframe = dataframe.map(map_fn2, num_proc=16)
             dataframes.append(dataframe)
         self.dataframe: datasets.Dataset = datasets.concatenate_datasets(dataframes)
 
         print(f"dataset len: {len(self.dataframe)}")
-
-    def map_fn(self, row: dict, *, data_source: str = None):
-        if data_source == "Maxwell-Jia/AIME_2024":
-            problem, answer = row["Problem"], row["Answer"]
-        elif data_source == "yentinglin/aime_2025":
-            problem, answer = row["problem"], row["answer"]
-
-        prompt = problem + answer_format
-        data = {
-            "data_source": data_source.split("/")[1].lower(),  # aime_2024, aime_2025
-            "prompt": [{"role": "user", "content": prompt}],
-            "ability": "MATH",
-            "reward_model": {"ground_truth": str(answer)},
-            "agent_name": "tool_agent",
-        }
-        return data
-
-    def map_fn2(self, row: dict):
-        content = row["prompt"][0]["content"]
-        row["prompt"][0]["content"] = content + answer_format
-        row["agent_name"] = "tool_agent"
-        return row
 
 
 def compute_score(data_source, solution_str, ground_truth, extra_info, **kwargs):
