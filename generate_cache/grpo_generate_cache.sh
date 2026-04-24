8c5f7117eb3954ca2d21434f7ba8c03028638602
sunsunsun98 <1270870143@qq.com>
2026-04-24 17:22:52 +0800

    Create grpo_generate_cache.sh

---
+++ generate_cache/grpo_generate_cache.sh
@@ -0,0 +1,84 @@
+set -x
+
+project_name='GRPO-model-dataset-generate_cache'
+exp_name='qwen3-8b-gsm8k-generate_cache'
+
+# export ASCEND_RT_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export HYDRA_FULL_ERROR=1
+
+export ASCEND_LAUNCH_BLOCKING=0
+
+export VLLM_USE_V1=1
+
+# Algorithm
+temperature=1.0
+top_p=1.0
+top_k=-1 # 0 for HF rollout, -1 for vLLM rollout
+
+# actor_rollout_ref.rollout.quantization=ascend \
+python3 -m verl.trainer.main_ppo \
+    algorithm.adv_estimator=grpo \
+    data.train_files=/data/l50044498/datasets/gsm8k_rl/train.parquet \
+    data.val_files=/data/l50044498/datasets/gsm8k_rl/test.parquet \
+    data.train_batch_size=256 \
+    data.max_prompt_length=512 \
+    data.max_response_length=1024 \
+    data.filter_overlong_prompts=True \
+    data.filter_overlong_prompts_workers=32 \
+    data.truncation='left' \
+    data.trust_remote_code=True \
+    +model.trust_remote_code=True \
+    actor_rollout_ref.model.path=/data/l50044498/models/Qwen3-8B \
+    actor_rollout_ref.actor.optim.lr=1e-6 \
+    actor_rollout_ref.actor.optim.lr_scheduler_type='constant' \
+    actor_rollout_ref.actor.optim.lr_warmup_steps=3 \
+    actor_rollout_ref.actor.optim.weight_decay=0.1 \
+    actor_rollout_ref.actor.optim.clip_grad=1.0 \
+    actor_rollout_ref.actor.grad_clip=1.0 \
+    actor_rollout_ref.actor.clip_ratio_low=0.2 \
+    actor_rollout_ref.actor.clip_ratio_high=0.28 \
+    actor_rollout_ref.actor.clip_ratio_c=10.0 \
+    actor_rollout_ref.model.use_remove_padding=False \
+    actor_rollout_ref.model.trust_remote_code=True \
+    actor_rollout_ref.actor.entropy_coeff=0 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=64 \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=8 \
+    actor_rollout_ref.actor.use_kl_loss=True \
+    actor_rollout_ref.actor.kl_loss_coef=0.001 \
+    actor_rollout_ref.actor.kl_loss_type=low_var_kl \
+    actor_rollout_ref.model.enable_gradient_checkpointing=True \
+    actor_rollout_ref.actor.fsdp_config.param_offload=True \
+    actor_rollout_ref.actor.fsdp_config.optimizer_offload=True \
+    actor_rollout_ref.rollout.load_format="auto" \
+    actor_rollout_ref.rollout.free_cache_engine=True \
+    actor_rollout_ref.rollout.enable_chunked_prefill=False \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
+    actor_rollout_ref.rollout.calculate_log_probs=True \
+    actor_rollout_ref.rollout.name=vllm \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.8 \
+    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=8 \
+    actor_rollout_ref.rollout.n=8 \
+    actor_rollout_ref.rollout.temperature=${temperature} \
+    actor_rollout_ref.rollout.top_p=${top_p} \
+    actor_rollout_ref.rollout.top_k=${top_k} \
+    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=8 \
+    actor_rollout_ref.ref.fsdp_config.param_offload=True \
+    algorithm.kl_ctrl.kl_coef=0 \
+    trainer.critic_warmup=0 \
+    trainer.logger='["console","tensorboard"]' \
+    trainer.rollout_data_dir='/home/l50044498/verl/history_data' \
+    trainer.gen_cache.use_gen_cache=True \
+    trainer.gen_cache.reuse_factor=0.1 \
+    trainer.gen_cache.save_path='/home/l50044498/verl/history_data' \
+    trainer.gen_cache.chunk_size=1000 \
+    trainer.project_name="${project_name}" \
+    trainer.experiment_name="${exp_name}" \
+    trainer.n_gpus_per_node=16 \
+    trainer.nnodes=1 \
+    trainer.save_freq=-1 \
+    trainer.test_freq=5 \
+    trainer.total_epochs=4 \
+    trainer.device=npu $@
+
+#     algorithm.rollout_correction.rollout_rs=${rollout_rs} \
+#     algorithm.rollout_correction.rollout_rs_threshold=${rollout_rs_threshold} \
